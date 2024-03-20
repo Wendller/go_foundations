00e3d82d2b26708e05b620efbe46bb02ab9fc20e
Wendler Tenorio <wendler_mendes@hotmail.com>
2024-03-20 00:38:20 -0300

    move folders to scope

---
+++ basics/serverMux/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/users", User{Name: "Wend"})
+
+	http.ListenAndServe(":8080", mux)
+}
+
+// Approach 1
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Mux"))
+}
+
+// Approach 2
+type User struct {
+	Name string
+}
+
+func (u User) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("User: " + u.Name))
+}
